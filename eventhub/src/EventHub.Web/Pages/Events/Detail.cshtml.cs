20da0d72545bca4a234b81ebbcba7e3bf87cba47
Halil İbrahim Kalkan <hi_kalkan@yahoo.com>
2021-01-24 13:54:18 +0300

    Redirect to true URL.

--- eventhub/src/EventHub.Web/Pages/Events/Detail.cshtml.cs
+++ eventhub/src/EventHub.Web/Pages/Events/Detail.cshtml.cs
@@ -2,6 +2,7 @@
 using EventHub.Events;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.AspNetCore.Routing;
 
 namespace EventHub.Web.Pages.Events
 {
@@ -13,17 +14,36 @@ namespace EventHub.Web.Pages.Events
         public EventDetailDto Event { get; set; }
 
         private readonly IEventAppService _eventAppService;
+        private readonly LinkGenerator _linkGenerator;
 
-        public Detail(IEventAppService eventAppService)
+        public Detail(
+            IEventAppService eventAppService,
+            LinkGenerator linkGenerator)
         {
             _eventAppService = eventAppService;
+            _linkGenerator = linkGenerator;
         }
 
-        public async Task OnGetAsync()
+        public async Task<IActionResult> OnGetAsync()
         {
             var urlCode = EventUrlCodeHelper.GetCodeFromUrl(Url);
 
             Event = await _eventAppService.GetByUrlCodeAsync(urlCode);
+
+            if (Event.Url != Url)
+            {
+                return RedirectPermanent(
+                    _linkGenerator.GetPathByPage(
+                        "/Events/Detail",
+                        values: new
+                        {
+                            url = Event.Url
+                        }
+                    )
+                );
+            }
+
+            return Page();
         }
     }
 }
