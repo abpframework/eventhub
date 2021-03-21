7c27607c2b806b1b0141512a4f444f9eacca78f5
Halil İbrahim Kalkan <hi_kalkan@yahoo.com>
2021-03-21 19:38:05 +0300

    Add EventHub.Admin.Application.Contracts project.

--- src/EventHub.Application.Contracts/EventHubApplicationContractsModule.cs
+++ src/EventHub.Application.Contracts/EventHubApplicationContractsModule.cs
@@ -1,23 +1,14 @@
-﻿using Volo.Abp.Account;
-using Volo.Abp.Identity;
+﻿using Volo.Abp.Application;
 using Volo.Abp.Modularity;
-using Volo.Abp.ObjectExtending;
-using Volo.Abp.PermissionManagement;
 
 namespace EventHub
 {
     [DependsOn(
         typeof(EventHubDomainSharedModule),
-        typeof(AbpAccountApplicationContractsModule),
-        typeof(AbpIdentityApplicationContractsModule),
-        typeof(AbpPermissionManagementApplicationContractsModule),
-        typeof(AbpObjectExtendingModule)
+        typeof(AbpDddApplicationContractsModule)
     )]
     public class EventHubApplicationContractsModule : AbpModule
     {
-        public override void PreConfigureServices(ServiceConfigurationContext context)
-        {
-            EventHubDtoExtensions.Configure();
-        }
+
     }
 }
