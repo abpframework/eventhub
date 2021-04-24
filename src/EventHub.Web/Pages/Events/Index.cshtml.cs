a8a87faffbebf4df162b839ede8fb6075bc4cfa0
Halil İbrahim Kalkan <hi_kalkan@yahoo.com>
2021-04-25 02:21:39 +0300

    Created empty events page

---
+++ src/EventHub.Web/Pages/Events/Index.cshtml.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace EventHub.Web.Pages.Events
+{
+    public class IndexModel : PageModel
+    {
+        public void OnGet()
+        {
+            
+        }
+    }
+}
