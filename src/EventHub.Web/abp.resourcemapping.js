43b40936629c848d968c146728903391dcdca7cb
Berkan Sasmaz <berkansasmazz@gmail.com>
2021-04-29 14:58:59 +0300

    Add package: daterangepicker

--- src/EventHub.Web/abp.resourcemapping.js
+++ src/EventHub.Web/abp.resourcemapping.js
@@ -1,8 +1,12 @@
 ﻿module.exports = {
     aliases: {
-        
+        "@node_modules": "./node_modules",
+        "@libs": "./wwwroot/libs"
     },
+    clean: [
+        "@libs"
+    ],
     mappings: {
-        
+        "@node_modules/daterangepicker/*": "@libs/date-range-picker/",
     }
 };
