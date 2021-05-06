cb8cacbea2164fe25c5ac0262eb8a242185d55e3
yusuf karatoprak <yusufkaratoprak@yahoo.com>
2021-05-06 15:37:28 +0300

    update for testing

---
+++ testingress.sh
@@ -0,0 +1,6 @@
+helm install nginx-ingress ingress-nginx/ingress-nginx \
+    --namespace ingress-basic \
+    --set controller.replicaCount=2 \
+    --set controller.nodeSelector."beta\.kubernetes\.io/os"=linux \
+    --set defaultBackend.nodeSelector."beta\.kubernetes\.io/os"=linux \
+    --set controller.admissionWebhooks.patch.nodeSelector."beta\.kubernetes\.io/os"=linux
