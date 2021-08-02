a15e2f4c6645b5d9c894ad9b58b2758fa32423a7
Halil İbrahim Kalkan <hi_kalkan@yahoo.com>
2021-08-02 09:03:51 +0300

    Update IEventAppService.cs

--- src/EventHub.Application.Contracts/Events/IEventAppService.cs
+++ src/EventHub.Application.Contracts/Events/IEventAppService.cs
@@ -1,8 +1,10 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Threading.Tasks;
 using Volo.Abp.Application.Dtos;
 using Volo.Abp.Application.Services;
+using Volo.Abp.SettingManagement;
 
 namespace EventHub.Events
 {
@@ -30,15 +32,16 @@ namespace EventHub.Events
     public class AddSessionDto
     {
         public Guid TrackId { get; set; }
-        
+        [Required]
+        [StringLength(SessionConsts.MaxTitleLength,
+            MinimumLength = SessionConsts.MinTitleLength)]
         public string Title { get; set; }
-
         public DateTime StartTime { get; set; }
-
         public DateTime EndTime { get; set; }
-
+        [Required]
+        [StringLength(SessionConsts.MaxDescriptionLength,
+            MinimumLength = SessionConsts.MinDescriptionLength)]
         public string Description { get; set; }
-
         public string Language { get; set; }
     }
 }
