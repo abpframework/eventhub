670852b2bb61b0f62b39b67010634e9024561c00
Halil İbrahim Kalkan <hi_kalkan@yahoo.com>
2021-08-10 11:14:17 +0300

    Install cert-manager

--- etc/k8s/scripts/azure/install-ingress.ps1
+++ etc/k8s/scripts/azure/install-ingress.ps1
@@ -47,3 +47,25 @@ helm install nginx-ingress ingress-nginx/ingress-nginx `
     --set defaultBackend.image.registry=$ACR_URL `
     --set defaultBackend.image.image=$DEFAULTBACKEND_IMAGE `
     --set defaultBackend.image.tag=$DEFAULTBACKEND_TAG
+
+# Label the ingress-basic namespace to disable resource validation
+kubectl label namespace ingress-basic cert-manager.io/disable-validation=true
+
+# Add the Jetstack Helm repository
+helm repo add jetstack https://charts.jetstack.io
+
+# Update your local Helm chart repository cache
+helm repo update
+
+# Install the cert-manager Helm chart
+helm install cert-manager jetstack/cert-manager `
+  --namespace ingress-basic `
+  --version ${CERT_MANAGER_TAG} `
+  --set installCRDs=true `
+  --set nodeSelector."kubernetes\.io/os"=linux `
+  --set image.repository=${ACR_URL}/${CERT_MANAGER_IMAGE_CONTROLLER} `
+  --set image.tag=${CERT_MANAGER_TAG} `
+  --set webhook.image.repository=${ACR_URL}/${CERT_MANAGER_IMAGE_WEBHOOK} `
+  --set webhook.image.tag=${CERT_MANAGER_TAG} `
+  --set cainjector.image.repository=${ACR_URL}/${CERT_MANAGER_IMAGE_CAINJECTOR} `
+  --set cainjector.image.tag=${CERT_MANAGER_TAG}
