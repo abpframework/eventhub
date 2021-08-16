ad5f4c1df137892a55d4fc5f0e151b40cdef7df7
Halil İbrahim Kalkan <hi_kalkan@yahoo.com>
2021-08-16 16:05:24 +0300

    Add versioning on azure deployment

--- etc/k8s/helm-chart/deploy-azure.ps1
+++ etc/k8s/helm-chart/deploy-azure.ps1
@@ -1,1 +1,3 @@
-helm upgrade --install eh-az eventhub -f .\eventhub\values.azure.yaml --namespace eventhub --create-namespace
+param ($version='latest')
+
+helm upgrade --install eh-az eventhub -f .\eventhub\values.azure.yaml --namespace eventhub --create-namespace --set global.eventHubImageVersion=$version
