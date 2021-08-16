6303c62b58dc022195b44e5da998c075b4369c2f
Halil İbrahim Kalkan <hi_kalkan@yahoo.com>
2021-08-16 16:37:33 +0300

    delete migrator job before helm upgrade

--- etc/k8s/helm-chart/deploy-azure.ps1
+++ etc/k8s/helm-chart/deploy-azure.ps1
@@ -1,3 +1,4 @@
 param ($version='latest')
 
+kubectl delete job eh-az-dbmigrator
 helm upgrade --install eh-az eventhub -f .\eventhub\values.azure.yaml --namespace eventhub --create-namespace --set global.eventHubImageVersion=$version
