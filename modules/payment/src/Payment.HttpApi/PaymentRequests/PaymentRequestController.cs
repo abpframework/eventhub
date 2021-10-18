1333306da8008d58e1dcfca335fab3b6a6639b58
enisn <info@enisnecipoglu.com>
2021-10-18 16:58:15 +0300

    Create PaymentRequestController.cs

---
+++ modules/payment/src/Payment.HttpApi/PaymentRequests/PaymentRequestController.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Threading.Tasks;
+using Volo.Abp;
+
+namespace Payment.PaymentRequests
+{
+	[RemoteService(Name = PaymentRemoteServiceConsts.RemoteServiceName)]
+    [Area("payment")]
+    [Route("api/payment/requests")]
+    public class PaymentRequestController : PaymentController, IPaymentRequestAppService
+    {
+        protected IPaymentRequestAppService PaymentRequestAppService { get; }
+
+        public PaymentRequestController(IPaymentRequestAppService paymentRequestAppService)
+        {
+            PaymentRequestAppService = paymentRequestAppService;
+        }
+
+        [HttpPost]
+        [Route("complete/{token}")]
+        public Task<PaymentRequestDto> CompleteAsync([FromRoute] string token)
+        {
+            return PaymentRequestAppService.CompleteAsync(token);
+        }
+
+        [HttpPost]
+        public Task<PaymentRequestDto> CreateAsync(PaymentRequestCreationDto input)
+        {
+            return PaymentRequestAppService.CreateAsync(input);
+        }
+
+        [HttpGet]
+        [Route("{id}")]
+        public Task<PaymentRequestDto> GetAsync(Guid id)
+        {
+            return PaymentRequestAppService.GetAsync(id);
+        }
+
+        [HttpPost]
+        [Route("start")]
+        public Task<StartPaymentResultDto> StartPaymentAsync(StartPaymentDto input)
+        {
+            return PaymentRequestAppService.StartPaymentAsync(input);
+        }
+    }
+}
