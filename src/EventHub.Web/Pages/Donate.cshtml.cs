c2613045775a7f5ec2676a8533c77228039553d8
enisn <info@enisnecipoglu.com>
2021-10-20 13:24:04 +0300

    Add a donation sample

---
+++ src/EventHub.Web/Pages/Donate.cshtml.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.AspNetCore.Http.Metadata;
+using Microsoft.AspNetCore.Mvc;
+using Payment.PaymentRequests;
+using Payment.Web.PaymentRequest;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Volo.Abp.Users;
+
+namespace EventHub.Web.Pages
+{
+    public class DonateModel : EventHubPageModel
+    {
+        private readonly IPaymentRequestAppService _paymentRequestAppService;
+        private readonly IPaymentUrlBuilder _paymentUrlBuilder;
+
+        public DonateModel(
+            IPaymentRequestAppService paymentRequestAppService, 
+            IPaymentUrlBuilder paymentUrlBuilder)
+        {
+            _paymentRequestAppService = paymentRequestAppService;
+            _paymentUrlBuilder = paymentUrlBuilder;
+        }
+
+        public decimal Amount { get; set; } = 5;
+
+        public async Task<IActionResult> OnPostAsync()
+        {
+            var paymentRequest = await _paymentRequestAppService.CreateAsync(new PaymentRequestCreationDto
+            {
+                CustomerId = CurrentUser.IsAuthenticated ? CurrentUser.Id.ToString() : Request.HttpContext.Connection.Id,
+                Price = Amount,
+                ProductId = "eventhub.donation",
+                ProductName = $"EventHub Donation | " + CurrentUser.UserName ?? Request.HttpContext.Connection.Id,
+            });
+
+            return Redirect(_paymentUrlBuilder.BuildCheckoutUrl(paymentRequest.Id).AbsoluteUri);
+        }
+    }
+}
