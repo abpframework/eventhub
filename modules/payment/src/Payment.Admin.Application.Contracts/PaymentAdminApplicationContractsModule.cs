8106fa7babfb2acac88829bda6860ce21da58e7f
Halil İbrahim Kalkan <hi_kalkan@yahoo.com>
2021-10-22 17:20:26 +0300

    Added empty admin application layers

---
+++ modules/payment/src/Payment.Admin.Application.Contracts/PaymentAdminApplicationContractsModule.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using Volo.Abp.Application;
+using Volo.Abp.Authorization;
+using Volo.Abp.Modularity;
+
+namespace Payment.Admin
+{
+    [DependsOn(
+        typeof(PaymentDomainSharedModule),
+        typeof(AbpDddApplicationContractsModule),
+        typeof(AbpAuthorizationModule)
+        )]
+    public class PaymentAdminApplicationContractsModule : AbpModule
+    {
+
+    }
+}
