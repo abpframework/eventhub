d0640c9f1cdd2a360c9e506ae652e89cb49e17c6
Halil İbrahim Kalkan <hi_kalkan@yahoo.com>
2021-10-22 19:39:37 +0300

    Added empty blazor UI project for the payment admin side.

---
+++ modules/payment/src/Payment.Admin.Blazor/PaymentAdminBlazorModule.cs
@@ -0,0 +1,21 @@
+﻿using Volo.Abp.AspNetCore.Components.Web.Theming.Routing;
+using Volo.Abp.AspNetCore.Components.WebAssembly.Theming;
+using Volo.Abp.Modularity;
+
+namespace Payment.Admin
+{
+    [DependsOn(
+        typeof(AbpAspNetCoreComponentsWebAssemblyThemingModule),
+        typeof(PaymentAdminHttpApiClientModule)
+        )]
+    public class PaymentAdminBlazorModule : AbpModule
+    {
+        public override void ConfigureServices(ServiceConfigurationContext context)
+        {
+            Configure<AbpRouterOptions>(options =>
+            {
+                options.AdditionalAssemblies.Add(typeof(PaymentAdminBlazorModule).Assembly);
+            });
+        }
+    }
+}
