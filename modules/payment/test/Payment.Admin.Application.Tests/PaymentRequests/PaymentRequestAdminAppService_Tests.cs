24934382fc0ec68e8185ff78ba770ff7d1a9eef0
Engincan VESKE <enginveske@gmail.com>
2021-10-29 21:37:14 +0300

    Add Payment.Admin.Application.Tests project

---
+++ modules/payment/test/Payment.Admin.Application.Tests/PaymentRequests/PaymentRequestAdminAppService_Tests.cs
@@ -0,0 +1,24 @@
+﻿using System.Threading.Tasks;
+using Payment.Admin.Payments;
+using Shouldly;
+using Xunit;
+
+namespace Payment.Admin.Application.Tests.PaymentRequests
+{
+    public class PaymentRequestAdminAppService_Tests : PaymentAdminApplicationTestBase
+    {
+        private readonly IPaymentRequestAdminAppService _paymentRequestAdminAppService;
+
+        public PaymentRequestAdminAppService_Tests()
+        {
+            _paymentRequestAdminAppService = GetRequiredService<IPaymentRequestAdminAppService>();
+        }
+
+        [Fact]
+        public async Task GetListAsync()
+        {
+            var paymentRequests = await _paymentRequestAdminAppService.GetListAsync(new PaymentRequestGetListInput());
+            paymentRequests.Items.ShouldNotBeNull();
+        }
+    }
+}
