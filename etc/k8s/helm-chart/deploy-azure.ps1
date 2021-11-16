b5b6905d19c2c2fa461f757f3b660085540a3901
Berkan Sasmaz <berkansasmazz@gmail.com>
2021-11-16 15:23:13 +0300

    Revert "Increase helm upgrade timeout"
    This reverts commit 75d23ccb42860b3767f8750a10909a48635d340b.

--- etc/k8s/helm-chart/deploy-azure.ps1
+++ etc/k8s/helm-chart/deploy-azure.ps1
@@ -1,4 +1,4 @@
 param ($version='latest')
 
 kubectl delete job eh-az-dbmigrator
-helm upgrade --timeout 600 --install eh-az eventhub -f .\eventhub\values.azure.yaml --namespace eventhub --create-namespace --set global.eventHubImageVersion=$version
+helm upgrade --install eh-az eventhub -f .\eventhub\values.azure.yaml --namespace eventhub --create-namespace --set global.eventHubImageVersion=$version
