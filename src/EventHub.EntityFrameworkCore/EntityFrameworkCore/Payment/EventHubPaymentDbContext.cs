33a45b56b2f5c167a99171bd59a2468e6fc8ee79
Halil İbrahim Kalkan <hi_kalkan@yahoo.com>
2021-11-20 20:24:32 +0300

    Update EventHubPaymentDbContext.cs

--- src/EventHub.EntityFrameworkCore/EntityFrameworkCore/Payment/EventHubPaymentDbContext.cs
+++ src/EventHub.EntityFrameworkCore/EntityFrameworkCore/Payment/EventHubPaymentDbContext.cs
@@ -8,6 +8,14 @@ using Volo.Abp.EntityFrameworkCore;
 
 namespace EventHub.EntityFrameworkCore.Payment
 {
+    /*
+     * This class replaces the IPaymentDbContext (defined by the Payment module)
+     * using the [ReplaceDbContext] attribute and implementing the IPaymentDbContext interface.
+     * It also declares the [ConnectionStringName] attribute to use the Payment connection string name
+     * instead of the Default in appsettings.json files.
+     * Finally, it calls modelBuilder.ConfigurePayment() extension method of the Payment module
+     * to configure the database mappings. 
+     */
     [ReplaceDbContext(typeof(IPaymentDbContext))]
     [ConnectionStringName(PaymentDbProperties.ConnectionStringName)]
     public class EventHubPaymentDbContext : AbpDbContext<EventHubPaymentDbContext>, IPaymentDbContext
