82193557bc1e1878b763b975faaed90f54bf34a0
Halil İbrahim Kalkan <hi_kalkan@yahoo.com>
2022-01-21 19:24:45 +0300

    Created renew-account-certificate.ps1

--- etc/azure/scripts/configure-dns.ps1
+++
@@ -1,2 +0,0 @@
-kubectl apply -f .\corednsms.yaml
-kubectl delete pod --namespace kube-system -l k8s-app=kube-dns
---
+++ etc/azure/scripts/renew-account-certificate.ps1
@@ -0,0 +1,7 @@
+kubectl delete -f .\corednsms.yaml --ignore-not-found=true
+kubectl delete pod --namespace kube-system -l k8s-app=kube-dns --ignore-not-found=true
+kubectl rollout status deployment --namespace kube-system coredns
+kubectl delete certificate eh-az-account-tls
+sleep 60
+kubectl apply -f .\corednsms.yaml
+kubectl delete pod --namespace kube-system -l k8s-app=kube-dns
