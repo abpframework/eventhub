92d260e1975a6a816b6ec0d084327a46c7a6ab40
Halil İbrahim Kalkan <hi_kalkan@yahoo.com>
2022-01-21 20:04:24 +0300

    Update renew-account-certificate.ps1

--- etc/azure/scripts/renew-account-certificate.ps1
+++ etc/azure/scripts/renew-account-certificate.ps1
@@ -1,7 +1,12 @@
+$currentFolder = $PSScriptRoot
+Set-Location $currentFolder
+
 kubectl delete -f .\corednsms.yaml --ignore-not-found=true
 kubectl delete pod --namespace kube-system -l k8s-app=kube-dns --ignore-not-found=true
 kubectl rollout status deployment --namespace kube-system coredns
 kubectl delete certificate eh-az-account-tls
+
 sleep 60
+
 kubectl apply -f .\corednsms.yaml
 kubectl delete pod --namespace kube-system -l k8s-app=kube-dns
