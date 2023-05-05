5636b49065e4e9095e0bb100d1ef77c605229b6d
selmankoc <selman.koc@volosoft.com>
2023-05-05 09:35:24 +0300

    fix cr name

--- etc/azure/scripts/push-images.ps1
+++ etc/azure/scripts/push-images.ps1
@@ -1,31 +1,31 @@
 param ($version='latest')
 
-az acr login --name volocr
+az acr login --name volosoft
 
-docker tag eventhub.dbmigrator:$version volocr.azurecr.io/eventhub.dbmigrator:$version
-docker push volocr.azurecr.io/eventhub.dbmigrator:$version
-docker tag volocr.azurecr.io/eventhub.dbmigrator:$version volocr.azurecr.io/eventhub.dbmigrator:latest
+docker tag eventhub.dbmigrator:$version volosoft.azurecr.io/eventhub.dbmigrator:$version
+docker push volosoft.azurecr.io/eventhub.dbmigrator:$version
+docker tag volosoft.azurecr.io/eventhub.dbmigrator:$version volosoft.azurecr.io/eventhub.dbmigrator:latest
 
-docker tag eventhub.www:$version volocr.azurecr.io/eventhub.www:$version
-docker push volocr.azurecr.io/eventhub.www:$version
-docker tag volocr.azurecr.io/eventhub.www:$version volocr.azurecr.io/eventhub.www:latest
+docker tag eventhub.www:$version volosoft.azurecr.io/eventhub.www:$version
+docker push volosoft.azurecr.io/eventhub.www:$version
+docker tag volosoft.azurecr.io/eventhub.www:$version volosoft.azurecr.io/eventhub.www:latest
 
-docker tag eventhub.api:$version volocr.azurecr.io/eventhub.api:$version
-docker push volocr.azurecr.io/eventhub.api:$version
-docker tag volocr.azurecr.io/eventhub.api:$version volocr.azurecr.io/eventhub.api:latest
+docker tag eventhub.api:$version volosoft.azurecr.io/eventhub.api:$version
+docker push volosoft.azurecr.io/eventhub.api:$version
+docker tag volosoft.azurecr.io/eventhub.api:$version volosoft.azurecr.io/eventhub.api:latest
 
-docker tag eventhub.admin:$version volocr.azurecr.io/eventhub.admin:$version
-docker push volocr.azurecr.io/eventhub.admin:$version
-docker tag volocr.azurecr.io/eventhub.admin:$version volocr.azurecr.io/eventhub.admin:latest
+docker tag eventhub.admin:$version volosoft.azurecr.io/eventhub.admin:$version
+docker push volosoft.azurecr.io/eventhub.admin:$version
+docker tag volosoft.azurecr.io/eventhub.admin:$version volosoft.azurecr.io/eventhub.admin:latest
 
-docker tag eventhub.admin-api:$version volocr.azurecr.io/eventhub.admin-api:$version
-docker push volocr.azurecr.io/eventhub.admin-api:$version
-docker tag volocr.azurecr.io/eventhub.admin-api:$version volocr.azurecr.io/eventhub.admin-api:latest
+docker tag eventhub.admin-api:$version volosoft.azurecr.io/eventhub.admin-api:$version
+docker push volosoft.azurecr.io/eventhub.admin-api:$version
+docker tag volosoft.azurecr.io/eventhub.admin-api:$version volosoft.azurecr.io/eventhub.admin-api:latest
 
-docker tag eventhub.account:$version volocr.azurecr.io/eventhub.account:$version
-docker push volocr.azurecr.io/eventhub.account:$version
-docker tag volocr.azurecr.io/eventhub.account:$version volocr.azurecr.io/eventhub.account:latest
+docker tag eventhub.account:$version volosoft.azurecr.io/eventhub.account:$version
+docker push volosoft.azurecr.io/eventhub.account:$version
+docker tag volosoft.azurecr.io/eventhub.account:$version volosoft.azurecr.io/eventhub.account:latest
 
-docker tag eventhub.background-services:$version volocr.azurecr.io/eventhub.background-services:$version
-docker push volocr.azurecr.io/eventhub.background-services:$version
-docker tag volocr.azurecr.io/eventhub.background-services:$version volocr.azurecr.io/eventhub.background-services:latest
+docker tag eventhub.background-services:$version volosoft.azurecr.io/eventhub.background-services:$version
+docker push volosoft.azurecr.io/eventhub.background-services:$version
+docker tag volosoft.azurecr.io/eventhub.background-services:$version volosoft.azurecr.io/eventhub.background-services:latest
